6a3959416b9147d791527852d3c1d2fc807bc498
codereader <greebo@angua.at>
2026-04-18 06:00:58 +0200

    Merge pull request #65 from klaussilveira/bug/6694

--- radiant/ui/UserInterfaceModule.cpp
+++ radiant/ui/UserInterfaceModule.cpp
@@ -84,6 +84,7 @@
 
 #include "ui/selectionset/SelectionSetToolmenu.h"
 #include "ui/brush/QuerySidesDialog.h"
+#include "ui/brush/CreateTrimDialog.h"
 #include "ui/brush/FindBrush.h"
 #include "ui/array/ArrayDialog.h"
 #include "ui/mousetool/RegistrationHelper.h"
@@ -570,6 +571,7 @@ void UserInterfaceModule::registerUICommands()
     GlobalCommandSystem().addCommand("ExportCollisionModelDialog", ExportCollisionModelDialog::Show);
     GlobalCommandSystem().addWithCheck("QueryBrushPrefabSidesDialog", QuerySidesDialog::Show,
                                        selection::pred::haveBrush, {cmd::ARGTYPE_INT});
+    GlobalCommandSystem().addCommand("CreateTrimDialog", CreateTrimDialog::CreateTrimCmd);
 
     // Set up the CloneSelection command to react on key up events only
     GlobalEventManager().addCommand("CloneSelection", "CloneSelection", true); // react on keyUp
